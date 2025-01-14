12dcc226e51d67db912abb50ed31539fdf2ead8e
okta26-hub <55014261+okta26-hub@users.noreply.github.com>
2025-01-14 16:16:50 +0700

    Update modifan.js

--- js/modifan.js
+++ js/modifan.js
@@ -145,7 +145,7 @@ function adjustGapHeight() {
 
     if (window.innerWidth <= 480) {
       // Untuk perangkat kecil (mobile)
-      gapElement.style.height = `${viewportHeight * 0.1}px`;
+      gapElement.style.height = `${viewportHeight * 0.01}px`;
     } else if (window.innerWidth <= 768) {
       // Untuk tablet
       gapElement.style.height = `${viewportHeight * 0.1}px`;
